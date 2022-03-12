8db5fe3b7980fe35a3d09e5a3f16ab4c0e7a2d7f
sfchampion <jucuo1905@126.com>
2022-03-12 14:47:01 +0800

    first commit

---
+++ src/main/java/com/sfchampion/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.sfchampion.git;
+
+/**
+ * @Author champion
+ * @Date 2022/3/12 14:41
+ * @Version 2020.3.4
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
